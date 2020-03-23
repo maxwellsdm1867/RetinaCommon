556304f1c8d4549f3642c74041220791a52145ae
maxwellsdm <maxwellsdm1867@gmail.com>
2020-03-23 17:19:01 +0800

    hmm Sr addition

--- stacked_plot.m
+++ stacked_plot.m
@@ -3,7 +3,7 @@ name_seq = cell(1,length(tar));
 hold on 
 for i = 1: length(tar)
     x = tar(i);
-    plot(input(x,:))
+    plot(input(x,:),'linewidth',1.2)
     name_seq{1,i}= num2str(x);
 end
 hold off
