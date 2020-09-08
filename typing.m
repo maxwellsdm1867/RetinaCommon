9e31bc3b052650e3a5a91fc6db773807a54e5340
arthurh <49833088+maxwellsdm1867@users.noreply.github.com>
2020-09-08 16:33:02 +0800

    cell type by spiking trigger average

---
+++ typing.m
@@ -0,0 +1,34 @@
+function [type_idx,res] = typing(sta)
+fact = 100;
+up_sta= interp(sta,fact);
+f_up= up_sta(1:1200);
+temp = f_up(800:end);
+m = mean(f_up(1:600));
+mid = temp-m;
+po = abs(sum(mid.*(mid>0)));
+ne = abs(sum(mid.*(mid<0)));
+tot_area = sum(abs(mid));
+d= po-ne;
+res = d./tot_area;
+com = mid(100:end)<0;
+k = diff(com);
+aa =find(k~=0);
+if~isempty(aa)
+    r = k(aa(1));
+else
+    r=87;
+end
+
+if res>0.8
+    type_idx = 1;%on cell
+elseif res<-0.8
+    type_idx = 2;%off cell
+elseif res<0.8 & res>-0.8 & r==1
+    type_idx = 3; %on off cell
+elseif res<0.8 & res>-0.8 & r==-1
+    type_idx = 4;%off on cell
+else
+    type_idx=5;%unindentify
+end
+    
+end
